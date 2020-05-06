be65a7b1a7553509d219f312b574af3fc53416d5
Luis Mayta <luis.mayta@drinux.com>
2020-05-06 16:15:38 -0500

    Portada 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ jQuery(function ($) {
 	var mapcanvas = document.getElementById("map-canvas");
 	if(mapcanvas){
 	  // Element exists
-		var coord = [40.738270, -74.008911]; // <--- coordinates here
+		var coord = [-12.0666003, -76.9588052]; // <--- coordinates here
 
 		var map = L.map('map-canvas', { scrollWheelZoom:false}).setView(coord, 18);
 
